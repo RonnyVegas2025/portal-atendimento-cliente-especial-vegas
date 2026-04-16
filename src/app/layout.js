14671c8cfbf12354cf8b7ab76467dcda6a0b882e
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-16 16:54:14 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,1 +1,15 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Portal Parceiros Vegas",
+  description: "Demo navegável do sistema de atendimento Vegas Card"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
 
