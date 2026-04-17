02923e034a48a2c84fba160c3a57d8592f816d9d
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-17 09:23:45 -0300

    Update format.js

--- src/lib/format.js
+++ src/lib/format.js
@@ -1,1 +1,7 @@
+export function onlyDigits(value = "") { return String(value).replace(/\D/g, ""); }
+export function formatCNPJ(value = "") {
+  const digits = onlyDigits(value).slice(0, 14);
+  return digits.replace(/^(\d{2})(\d)/, "$1.$2").replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3").replace(/\.(\d{3})(\d)/, ".$1/$2").replace(/(\d{4})(\d)/, "$1-$2");
+}
+export function formatDateTime(value) { if (!value) return "-"; try { return new Date(value).toLocaleString("pt-BR"); } catch { return value; } }
 
