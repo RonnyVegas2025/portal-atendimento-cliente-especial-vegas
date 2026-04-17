a0cc78346b30292c619b625a9421a8023c6b0f80
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-04-17 11:48:47 -0300

    Update ProtectedPage.js

--- src/components/ProtectedPage.js
+++ src/components/ProtectedPage.js
@@ -1,1 +1,20 @@
+"use client";
 
+import { useRequireAuth } from "@/hooks/useRequireAuth";
+import { Card } from "@/components/UI";
+
+export default function ProtectedPage({ children, allowedRoles = [] }) {
+  const { loading } = useRequireAuth(allowedRoles);
+
+  if (loading) {
+    return (
+      <div className="p-6">
+        <Card>
+          <div className="p-6 text-sm text-slate-500">Carregando acesso...</div>
+        </Card>
+      </div>
+    );
+  }
+
+  return children;
+}
